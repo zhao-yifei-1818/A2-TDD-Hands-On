2543c096dedfff51ab00003ba9984fa73af7ff66
Yifei Zhao <49413602+zhao-yifei-1818@users.noreply.github.com>
2026-05-11 16:46:44 -0700

    initial commit

---
+++ test.py
@@ -0,0 +1,29 @@
+import unittest
+from task import check_pwd
+
+'''check_pwd accepts a string and returns a boolean: True if it meets the criteria listed below, otherwise it returns False:
+
+Must be between 8 and 20 characters (inclusive of both bounds)
+Must contain at least one lowercase letter (standard English alphabet)
+Must contain at least one uppercase letter (standard English alphabet)
+Must contain at least one digit
+Must contain at least one symbol from: ~`!@#$%^&*()_+-= (copy and paste to avoid missing characters) These are the only symbols that will meet this requirement. Other symbols may be present, but they won't satisfy this requirement.'''
+class TestCase(unittest.TestCase):
+    
+    def test1(self):
+        input = "Password123!"
+        expected = True
+        self.assertEqual(check_pwd(input), expected)
+    ''' 
+    def test2(self):
+        input = "Password"
+        expected = False
+        self.assertEqual(check_pwd(input), expected)
+    
+    def test3(self):
+        input = "Password1234567890!"
+        expected = True
+        self.assertEqual(check_pwd(input), expected)
+'''
+if __name__ == '__main__':
+    unittest.main()
