702d6787c099d3a5f51168ad82811b0051412ef1
zhao-yifei-1818 <williamfei0000@gmail.com>
2026-05-12 23:40:30 -0700

    stage 3 lowercase R

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ from check_pwd import check_pwd
 
 '''check_pwd accepts a string and returns a boolean: True if it meets the criteria listed below, otherwise it returns False:
 
-Must be between 8 and 20 characters (inclusive of both bounds)
 Must contain at least one lowercase letter (standard English alphabet)
 Must contain at least one uppercase letter (standard English alphabet)
 Must contain at least one digit
@@ -21,8 +20,18 @@ class TestCase(unittest.TestCase):
         self.assertEqual(check_pwd(input), expected)
 
     def test3(self):
-        input = "Passwordpasswordpas"
+        input = "Passwordpasswordpass"
         expected = True
+        self.assertEqual(check_pwd(input), expected)
+
+    def test4(self):
+        input = "Passwordpasswordpassw"
+        expected = False
+        self.assertEqual(check_pwd(input), expected)
+
+    def test5(self):
+        input = "PASSWORD"
+        expected = False
         self.assertEqual(check_pwd(input), expected)
     
 if __name__ == '__main__':
