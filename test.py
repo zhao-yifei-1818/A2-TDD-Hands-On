341b0be3f8d8109fb0c24a3d3913ed5d3823b41a
zhao-yifei-1818 <williamfei0000@gmail.com>
2026-05-13 22:44:34 -0700

    cleaning up

--- test.py
+++ test.py
@@ -1,12 +1,8 @@
 import unittest
 from check_pwd import check_pwd
 
-'''check_pwd accepts a string and returns a boolean: True if it meets the criteria listed below, otherwise it returns False:
 
-Must contain at least one digit
-Must contain at least one symbol from: ~`!@#$%^&*()_+-= (copy and paste to avoid missing characters) These are the only symbols that will meet this requirement. Other symbols may be present, but they won't satisfy this requirement.'''
 class TestCase(unittest.TestCase):
-
     def test1(self):
         input = ""
         expected = False
@@ -26,12 +22,12 @@ class TestCase(unittest.TestCase):
         input = "PASSWORDPASSWORD1234"
         expected = False
         self.assertEqual(check_pwd(input), expected)
-    
+
     def test5(self):
         input = "passwordpassword1234"
         expected = False
         self.assertEqual(check_pwd(input), expected)
-    
+
     def test6(self):
         input = "Passwordpassword"
         expected = False
@@ -52,5 +48,6 @@ class TestCase(unittest.TestCase):
         expected = True
         self.assertEqual(check_pwd(input), expected)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
